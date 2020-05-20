2382fce00770047d6fd27809f56071d6e20929cb
Bluette1 <marylene.sawyer@gmail.com>
2020-05-20 17:53:37 +0200

    Fixing lint errors
    - Fixing lint errors

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -4,30 +4,29 @@
 # position of the last index.
 
 def bubble_sort_by(array)
-    size = array.length - 2
-  
-    while size >= 0
-  
-      (0..size).each do |i|
-      result=yield array[i],array[i + 1]
-      next unless result > 0
-  
-        # swap the values
-        temp = array[i]
-        array[i] = array[i + 1]
-        array[i + 1] = temp
-      end
-  
-      size -= 1
-  
-    end
-  
-    array
-end
-  array = ["hi","hello","hey"]
-result= 
-  bubble_sort_by(["hi","hello","hey"]) do |left,right|
-    left.length - right.length
+  size = array.length - 2
 
+  while size >= 0
+
+    (0..size).each do |i|
+      result = yield array[i], array[i + 1]
+      next unless result.positive?
+
+      # swap the values
+      temp = array[i]
+      array[i] = array[i + 1]
+      array[i + 1] = temp
+    end
+
+    size -= 1
+
+  end
+
+  array
 end
+
+result =
+  bubble_sort_by(%w[hi hello hey]) do |left, right|
+    left.length - right.length
+  end
 print(result)
